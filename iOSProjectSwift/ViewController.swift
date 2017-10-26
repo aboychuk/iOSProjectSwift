c71e2045ddc7d75bf4f2963c00be530b30ca9bdd
Andrew Boychuk <andrewboychuk@gmail.com>
2017-10-26 18:02:52 +0300

    Initial Commit

---
+++ iOSProjectSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSProjectSwift
+//
+//  Created by Andrew Boychuk on 10/26/17.
+//  Copyright © 2017 Andrew Boychuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
