2ec31fb13eb35f9c1ac1c28f09c888176e8e18cd
Andrew Boychuk <andrewboychuk@gmail.com>
2017-11-03 20:29:51 +0200

    BaseModel added.

---
+++ iOSProjectSwift/Source/Models/BaseModel/BaseModel.swift
@@ -0,0 +1,22 @@
+//
+//  BaseModel.swift
+//  iOSProjectSwift
+//
+//  Created by Andrew Boychuk on 11/3/17.
+//  Copyright © 2017 Andrew Boychuk. All rights reserved.
+//
+
+import Foundation
+
+enum ModelObserver {
+    case modelDidUnload
+    case modelWillLoad
+    case modelDidLoad
+    case modelFailLoad
+}
+
+class BaseModel: NSObject {
+    func load() {
+        let state = 
+    }
+}
