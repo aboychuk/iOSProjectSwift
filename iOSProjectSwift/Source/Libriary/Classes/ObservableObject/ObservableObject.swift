719301788c8ea93d91109c874d6307248e6b666a
Andrew Boychuk <andrewboychuk@gmail.com>
2017-11-03 20:33:31 +0200

    ObservableObject added.

---
+++ iOSProjectSwift/Source/Libriary/Classes/ObservableObject/ObservableObject.swift
@@ -0,0 +1,9 @@
+//
+//  ObservableObject.swift
+//  iOSProjectSwift
+//
+//  Created by Andrew Boychuk on 11/3/17.
+//  Copyright © 2017 Andrew Boychuk. All rights reserved.
+//
+
+import Foundation
