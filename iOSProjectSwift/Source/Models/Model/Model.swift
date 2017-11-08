e2151469b39ae3d52753a26139add3bf276a9529
Andrew Boychuk <andrewboychuk@gmail.com>
2017-11-08 14:36:22 +0200

    Model class implemented.

--- iOSProjectSwift/Source/Models/Model/Model.swift
+++ iOSProjectSwift/Source/Models/Model/Model.swift
@@ -8,6 +8,13 @@
 
 import Foundation
 
+protocol ModelObserver {
+    func didUnload(model: Model)
+    func willLoad(model: Model)
+    func didLoad(model: Model)
+    func didFailLoading(model: Model)
+}
+
 enum ModelState {
     case didUnload
     case willLoad
@@ -15,10 +22,46 @@ enum ModelState {
     case didFailLoading
 }
 
-class BaseModel: ObservableObject {
+class Model: ObservableObject {
+    
+    //MARK: - Public Methods
+    
     func load() {
         let state = self.state
         if state == .willLoad || state == .didLoad {
+            self.notify(of: state)
+            return
+        }
+        self.state = .willLoad
+        self.processLoading()
+    }
+    
+    //method created for overriding, do not call directly
+    func performLoadingInBackground() {
+        
+    }
+    
+    //MARK: - Private Methods
+    
+    private func processLoading() {
+        let backgroundQueue = DispatchQueue.global(qos: .background)
+        backgroundQueue.async {
+            self.performLoadingInBackground()
+        }
+    }
+    
+    //MARK: - Overrided Methods
+    
+    override func selector(for state: ModelState) -> Selector? {
+        switch state {
+        case .didUnload:
+            return Selector(("didUnload:"))
+        case .willLoad:
+            return Selector(("willLoad:"))
+        case .didLoad:
+            return Selector(("didLoad:"))
+        case .didFailLoading:
+            return Selector(("didFailLoading:"))
         }
     }
 }
