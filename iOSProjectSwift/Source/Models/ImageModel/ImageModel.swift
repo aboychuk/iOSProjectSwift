bf13c4c8bffc9b5c62010b50460cab98afee929c
Andrew Boychuk <andrewboychuk@gmail.com>
2017-11-09 18:20:41 +0200

    ImageModel -> initial commit.

---
+++ iOSProjectSwift/Source/Models/ImageModel/ImageModel.swift
@@ -0,0 +1,30 @@
+//
+//  ImageModel.swift
+//  iOSProjectSwift
+//
+//  Created by Andrew Boychuk on 11/9/17.
+//  Copyright © 2017 Andrew Boychuk. All rights reserved.
+//
+
+import UIKit
+
+class ImageModel: Model {
+    
+    //MARK: - Properties
+    
+    var image: UIImage?
+    let url: URL
+    
+    //MARK: - Initializers
+    
+    init(url: URL) {
+        self.url = url
+        
+        super.init()
+    }
+    
+    //MARK: - Public Methods
+    
+    func
+    
+}
