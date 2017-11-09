f292c649295ff3c24b9fc577ba788dc6a4adad88
Andrew Boychuk <andrewboychuk@gmail.com>
2017-11-09 18:21:14 +0200

    FileSystemImageModel, InternetImageModel -> initial commit.

---
+++ iOSProjectSwift/Source/Models/ImageModel/FileSystemImageModel/FileSystemImageModel.swift
@@ -0,0 +1,9 @@
+//
+//  FileSystemImageModel.swift
+//  iOSProjectSwift
+//
+//  Created by Andrew Boychuk on 11/9/17.
+//  Copyright © 2017 Andrew Boychuk. All rights reserved.
+//
+
+import Foundation
---
+++ iOSProjectSwift/Source/Models/ImageModel/InternetImageModel/InternetImageModel.swift
@@ -0,0 +1,9 @@
+//
+//  InternetImageModel.swift
+//  iOSProjectSwift
+//
+//  Created by Andrew Boychuk on 11/9/17.
+//  Copyright © 2017 Andrew Boychuk. All rights reserved.
+//
+
+import Foundation
