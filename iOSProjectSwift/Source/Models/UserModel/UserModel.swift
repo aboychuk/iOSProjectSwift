af250555c68c11ddc5da277b98974a9109827dc6
Andrew Boychuk <andrewboychuk@gmail.com>
2017-11-17 16:42:24 +0200

    UserModel -> NSCoding added, fullname refactored.

--- iOSProjectSwift/Source/Models/UserModel/UserModel.swift
+++ iOSProjectSwift/Source/Models/UserModel/UserModel.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class UserModel: Model {
     
+    struct Keys {
+        static let name = "UserName"
+        static let surname = "UserSurname"
+    }
+    
     //MARK: - Properties
     
     var name: String?
     var surname: String?
     var fullname: String? {
-        return 
+        if let name = self.name {
+            return self.surname == nil ? name : "\(name)  \(self.surname ?? "")"
+        }
+        
+        return nil
+    }
+    
+    // MARK: - NSCoding
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(self.name, forKey:Keys.name)
+        aCoder.encode(self.surname, forKey:Keys.surname)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        self.name = aDecoder.decodeObject(forKey: Keys.name) as? String
+        self.surname = aDecoder.decodeObject(forKey: Keys.surname) as? String
     }
 }
