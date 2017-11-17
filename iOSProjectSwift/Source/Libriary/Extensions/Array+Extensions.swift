13381cb047d59bb696f7f8b865b1019d5492914b
Andrew Boychuk <andrewboychuk@gmail.com>
2017-11-17 10:26:48 +0200

    Array+Extension, UITableView+Extension initial commit.

---
+++ iOSProjectSwift/Source/Libriary/Extensions/Array+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Array+Extensions.swift
+//  iOSProjectSwift
+//
+//  Created by Andrew Boychuk on 11/16/17.
+//  Copyright © 2017 Andrew Boychuk. All rights reserved.
+//
+
+import UIKit
---
+++ iOSProjectSwift/Source/Libriary/Extensions/UITableView+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  UITableView+Extensions.swift
+//  iOSProjectSwift
+//
+//  Created by Andrew Boychuk on 11/17/17.
+//  Copyright © 2017 Andrew Boychuk. All rights reserved.
+//
+
+import UIKit
