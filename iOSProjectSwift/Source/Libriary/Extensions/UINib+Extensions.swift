20d87d01453d4509bcb8f1020172e82da722cb22
Andrew Boychuk <andrewboychuk@gmail.com>
2017-11-17 11:33:44 +0200

    UINib+Extensions -> initial commit.

---
+++ iOSProjectSwift/Source/Libriary/Extensions/UINib+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  UINib+Extensions.swift
+//  iOSProjectSwift
+//
+//  Created by Andrew Boychuk on 11/17/17.
+//  Copyright © 2017 Andrew Boychuk. All rights reserved.
+//
+
+import Foundation
