b0e54ac5816e7711c35b6d9f1d0008770628d9ba
Andrew Boychuk <andrewboychuk@gmail.com>
2017-11-29 18:18:53 +0200

    LoadingView -> implemented.

--- iOSProjectSwift/Source/UI/Views/LoadingView/LoadingView.swift
+++ iOSProjectSwift/Source/UI/Views/LoadingView/LoadingView.swift
@@ -9,37 +9,70 @@
 import UIKit
 
 class LoadingView: View {
+    
+    typealias VoidBLock = () -> ()
+    
+    private struct Constants {
+        static let defaultDuration: Double = 3
+        static let visibleAlpha: CGFloat = 1
+        static let invisibleAlpha: CGFloat = 0.0
+        static let defaultAlpha: CGFloat = 0.5
+    }
 
     //MARK: - Properties
     
     @IBOutlet var activityIndicator: UIActivityIndicatorView?
     
-//    var visible: Bool
+    var visible: Bool = false
     
     //MARK: - Initializers
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-    }
+        self.prepareView()
+        }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     //MARK: - Public functions
+    
+    func loadingView(in superView: UIView) -> LoadingView {
+        return LoadingView(frame: superView.bounds)
+    }
 
-    func set(visible: Bool, animated: Bool, on completion:()->()) {
+    func set(visible: Bool, animated: Bool = true, on completionHandler: VoidBLock? = nil) {
+        UIView.animate(withDuration: animated ? Constants.defaultDuration : 0, animations: { [weak self] in
+                        self?.alpha = visible ? Constants.visibleAlpha : Constants.invisibleAlpha
+        }) { [weak self](_) in
+            completionHandler?()
+        }
         
+        self.visible = visible
     }
     
     //MARK: - Private functions
     
     func prepareView() {
-        self.autoresizingMask = 
-        
+        self.autoresizingMask = .autoresizeWithFixedPosition
+        self.backgroundColor = UIColor.init(white: 0, alpha: Constants.defaultAlpha)
+        self.prepareActivityIndicator()
     }
     
     func prepareActivityIndicator() {
-        
+        if var indicator = self.activityIndicator {
+            if (indicator != nil) {
+                self.activityIndicator = indicator
+                self.addSubview(self.activityIndicator!)
+            } else {
+                indicator = UIActivityIndicatorView(activityIndicatorStyle: .white)
+                indicator.autoresizingMask = .autoresize
+                indicator.center = self.center
+                indicator.startAnimating()
+                self.activityIndicator = indicator
+                self.addSubview(self.activityIndicator!)
+            }
+        }
     }
 }
