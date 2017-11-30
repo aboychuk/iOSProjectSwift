5a27716a7b3112bbe2644393aba4d4dc38b15b51
Andrew Boychuk <andrewboychuk@gmail.com>
2017-11-30 11:48:35 +0200

    ImageView -> base implementation.

--- iOSProjectSwift/Source/UI/Views/ImageView/ImageView.swift
+++ iOSProjectSwift/Source/UI/Views/ImageView/ImageView.swift
@@ -6,4 +6,42 @@
 //  Copyright © 2017 Andrew Boychuk. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ImageView: View {
+    
+    //MARK: - Properties
+    
+    var imageModel: ImageModel?
+    var contentImageView: UIImageView?
+    
+    //MARK: - Initializations
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.prepareContentImageView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.prepareContentImageView()
+    }
+    
+    //MARK: - Public functions
+    
+    func fill(with model: ImageModel) {
+        self.contentImageView?.image = model.image
+    }
+    
+    //MARK: - Private functions
+    
+    private func prepareContentImageView() {
+        let imageView = UIImageView(frame: self.bounds)
+        imageView.autoresizingMask = .autoresize
+        self.contentImageView = imageView
+    }
+}
