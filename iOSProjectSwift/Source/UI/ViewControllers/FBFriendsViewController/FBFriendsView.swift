891d39b7c0dcd8d446e2ceb7bc2bca0073a076b7
Andrew Boychuk <andrewboychuk@gmail.com>
2017-11-30 22:44:20 +0200

    FBFriendsView, FBFriendsViewController -> base implementation added.

---
+++ iOSProjectSwift/Source/UI/ViewControllers/FBFriendsViewController/FBFriendsView.swift
@@ -0,0 +1,23 @@
+//
+//  FBFriendsView.swift
+//  iOSProjectSwift
+//
+//  Created by Andrew Boychuk on 11/30/17.
+//  Copyright © 2017 Andrew Boychuk. All rights reserved.
+//
+
+import UIKit
+
+class FBFriendsView: BaseView {
+    
+    //MARK: - Properties
+    
+    @IBOutlet var tableview: UITableView?
+    
+    //MARK: - Public functions
+    
+    func fillWithModel {
+        self.tableview?.reloadData()
+    }
+    
+}
--- iOSProjectSwift/Source/UI/ViewControllers/FBFriendsViewController/FBFriendsViewController.swift
+++ iOSProjectSwift/Source/UI/ViewControllers/FBFriendsViewController/FBFriendsViewController.swift
@@ -6,4 +6,41 @@
 //  Copyright © 2017 Andrew Boychuk. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class FBFriendsViewController: FBViewController, UITableViewDelegate, UITableViewDataSource  {
+    
+    //MARK: - UITableViewDataSource protocol
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return self.model.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+    
+    //MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.prepareNavigationTitle()
+        if let friendsModel = self.model {
+            self.context = FBFriendsContext(model: friendsModel)
+        }
+    }
+    
+    //MARK: - Public functions
+    
+    override func updateWithModel(_ model: Model) {
+//        self.rootView.fillWithModel
+    }
+    
+    //MARK: - Private functions
+    
+    private func prepareNavigationTitle() {
+        self.navigationItem.title = "Friends"
+    }
+
+    
+}
