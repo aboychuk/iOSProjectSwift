dc78f6a998631037e69c8d51f89556a82d61bcd5
Andrew Boychuk <andrewboychuk@gmail.com>
2017-11-30 17:19:56 +0200

    FBLoginView, FBLoginView.xib initial commit.

---
+++ iOSProjectSwift/Source/UI/ViewControllers/FBLoginViewController/FBLoginView.swift
@@ -0,0 +1,17 @@
+//
+//  FBLoginView.swift
+//  iOSProjectSwift
+//
+//  Created by Andrew Boychuk on 11/30/17.
+//  Copyright © 2017 Andrew Boychuk. All rights reserved.
+//
+
+import UIKit
+
+class FBloginView: BaseView {
+    
+    //MARK: - Properties
+    
+    @IBOutlet var loginButton: UIButton?
+    
+}
