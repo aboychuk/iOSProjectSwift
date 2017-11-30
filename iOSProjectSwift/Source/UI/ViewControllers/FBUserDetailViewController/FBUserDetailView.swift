c1bc3e333fa08a90102c36f3761bc68a9523632e
Andrew Boychuk <andrewboychuk@gmail.com>
2017-11-30 18:43:20 +0200

    FBUserDetailView, FBUserDetailView.xib -> initial commit.

---
+++ iOSProjectSwift/Source/UI/ViewControllers/FBUserDetailViewController/FBUserDetailView.swift
@@ -0,0 +1,25 @@
+//
+//  FBUserDetailView.swift
+//  iOSProjectSwift
+//
+//  Created by Andrew Boychuk on 11/30/17.
+//  Copyright © 2017 Andrew Boychuk. All rights reserved.
+//
+
+import UIKit
+
+class FBUserDetailView: BaseView {
+    
+    //MARK: - Properties
+    
+    @IBOutlet var friendsButton: UIButton?
+    @IBOutlet var imageView: ImageView?
+    @IBOutlet var userFullName: UILabel?
+    
+    //MARK: - Public functions
+    
+    func fillWithModel(_ model: FBUserModel) {
+        self.userFullName?.text = model.fullname
+        self.imageView?.imageModel = model.image
+    }
+}
--- iOSProjectSwift/Source/UI/ViewControllers/FBUserDetailViewController/FBUserDetailViewController.swift
+++ iOSProjectSwift/Source/UI/ViewControllers/FBUserDetailViewController/FBUserDetailViewController.swift
@@ -6,4 +6,50 @@
 //  Copyright © 2017 Andrew Boychuk. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class FBUserDetailController : FBViewController {
+    
+    //MARK: - RootView protocol
+    
+    typealias ViewType = FBUserDetailView
+    
+    //MARK: - Properties
+    
+    var logoutContext: FBLogoutContext? {
+        willSet { newValue?.execute() }
+        didSet { oldValue?.cancel() }
+    }
+    
+    //MARK: - IBActions
+    
+    @IBAction func onFriends(sender: UIButton) {
+        
+    }
+    
+    @IBAction func onLogout(sender: UIButton) {
+        
+    }
+    
+    //MARK: View Lyfecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.prepareNavigationItem()
+        if let model = self.model {
+            self.context = FBUserDetailContext.init(model: model)
+        }
+    }
+    
+    //MARK: Public functions
+    
+    func updateWithModel(_ model: Model) {
+//        self.rootView.fillWithModel
+    }
+    
+    //MARK: Private functions
+    
+    private func prepareNavigationItem() {
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Logout", style: .done, target: self, action: #selector(onLogout(sender:)))
+    }
+}
