8b152caead35e3104d0460cd948f8cc2571b49d9
Andrew Boychuk <andrewboychuk@gmail.com>
2017-12-01 19:38:46 +0200

    ObservableObject -> refactored.

--- iOSProjectSwift/Source/Library/Classes/ObservableObject/ObservableObject.swift
+++ iOSProjectSwift/Source/Library/Classes/ObservableObject/ObservableObject.swift
@@ -8,75 +8,75 @@
 
 import UIKit
 
-//extension ObservableObject {
-//    class ObservationController {
-//        typealias Observer = AnyObject
-//        typealias Action = (ObservableObject) -> ()
-//
-//        //MARK: - Private properties
-//
-//        private var observer : Observer
-//        private var observableObject : ObservableObject
-//        private var relation = [ModelState : Action]()
-//
-//        //MARK: - Initialization
-//
-//        init(observableObject: ObservableObject, observer: Observer) {
-//            self.observableObject = observableObject
-//            self.observer = observer
-//        }
-//
-//        //MARK: - Public functions
-//
-//        func notify(of state: ModelState) {
-//            if let block = self.relation[state] {
-//                block(self.observableObject)
-//            }
-//        }
-//
-//        subscript(state: ModelState) -> Action? {
-//            get {
-//                return self.relation[state]
-//            }
-//
-//            set {
-//                self.relation[state] = newValue
-//            }
-//        }
-//    }
-//}
-//
-//class ObservableObject {
-//
-//    // MARK: - Properties
-//
-//    var state: ModelState = .didUnload {
-//        didSet {
-//            self.notifyOfState(self.state)
-//        }
-//    }
-//
-//    var observationControllers: NSHashTable<ObservationController> = NSHashTable.weakObjects()
-//    var notify: Bool = true
-//
-//    // MARK: - Public Functions
-//
-//    func controller(for observer: AnyObject) -> ObservationController {
-//        let controller = ObservationController(observableObject: self, observer: observer)
-//        self.observationControllers.add(controller)
-//
-//        return controller
-//
-//    }
-//
-//    func notifyOfState(_ state : ModelState) {
-//        synchronized(self) {
-//            self.observationControllers.allObjects.forEach{
-//                $0.notify(of: state)
-//            }
-//        }
-//    }
-//}
+class ObservableObject {
+    
+    // MARK: - Properties
+    
+    var state: ModelState = .didUnload {
+        didSet {
+            self.notifyOfState(self.state)
+        }
+    }
+    
+    var observationControllers: NSHashTable<ObservationController> = NSHashTable.weakObjects()
+    var notify: Bool = true
+    
+    // MARK: - Public Functions
+    
+    func controller(for observer: AnyObject) -> ObservationController {
+        let controller = ObservationController(observableObject: self, observer: observer)
+        self.observationControllers.add(controller)
+        
+        return controller
+        
+    }
+    
+    func notifyOfState(_ state : ModelState) {
+        synchronized(self) {
+            self.observationControllers.allObjects.forEach {
+                $0.notify(of: state)
+            }
+        }
+    }
+}
+
+extension ObservableObject {
+    class ObservationController {
+        typealias Observer = AnyObject
+        typealias Action = (ObservableObject) -> ()
+
+        //MARK: - Private properties
+
+        private var observer : Observer
+        private var observableObject : ObservableObject
+        private var relation = [ModelState : Action]()
+
+        //MARK: - Initialization
+
+        init(observableObject: ObservableObject, observer: Observer) {
+            self.observableObject = observableObject
+            self.observer = observer
+        }
+
+        //MARK: - Public functions
+
+        func notify(of state: ModelState) {
+            if let block = self.relation[state] {
+                block(self.observableObject)
+            }
+        }
+
+        subscript(state: ModelState) -> Action? {
+            get {
+                return self.relation[state]
+            }
+
+            set {
+                self.relation[state] = newValue
+            }
+        }
+    }
+}
 
 //
 //  ObservableObject.swift
@@ -86,76 +86,76 @@ import UIKit
 //  Copyright © 2017 Andrew Boychuk. All rights reserved.
 
 
-import UIKit
-
-class ObservableObject {
-    
-    // MARK: - Properties
-    
-    var state: ModelState = .didUnload {
-        didSet {
-            self.notify(of: self.state)
-        }
-    }
-    
-    var observers: NSHashTable<AnyObject> = NSHashTable.weakObjects()
-    var notify: Bool = true
-    
-    // MARK: - Public Functions
-    
-    func add(observer: AnyObject) {
-        synchronized(self) {
-            self.observers.add(observer)
-        }
-    }
-    
-    func remove(observer: AnyObject) {
-        synchronized(self) {
-            self.observers.remove(observer)
-        }
-    }
-    
-    func isObserved(by object: AnyObject) -> Bool {
-        return synchronized(self, block: { () -> (Bool) in
-            return self.observers.contains(object)
-        })
-    }
-    
-    func notify(of state: ModelState) {
-        synchronized(self) {
-            self.notifyWith(self.selector(for: state))
-        }
-    }
-    
-    func notifyOfState(_ state: ModelState, with object: AnyObject) {
-        synchronized(self) {
-            self.notifyWith(self.selector(for: state), with: object)
-        }
-    }
-    
-    func perform(notification: Bool, block: () -> ()) {
-        let notify = self.notify
-        self.notify = notification
-        block()
-        self.notify = notify
-    }
-    
-    //Method created for overriding, do not call directly
-    func selector(for state: ModelState) -> Selector? {
-        return nil
-    }
-    
-    // MARK: - Private Functions
-    
-    private func notifyWith(_ selector: Selector?, with object: AnyObject? = nil) {
-        if self.notify {
-            self.observers.allObjects.forEach({ (object) in
-                if (object.responds(to: selector)) {
-                    _ = object.perform(selector, with: object)
-                }
-            })
-        }
-    }
-}
-
-
+//import UIKit
+//
+//class ObservableObject {
+//
+//    // MARK: - Properties
+//
+//    var state: ModelState = .didUnload {
+//        didSet {
+//            self.notify(of: self.state)
+//        }
+//    }
+//
+//    var observers: NSHashTable<AnyObject> = NSHashTable.weakObjects()
+//    var notify: Bool = true
+//
+//    // MARK: - Public Functions
+//
+//    func add(observer: AnyObject) {
+//        synchronized(self) {
+//            self.observers.add(observer)
+//        }
+//    }
+//
+//    func remove(observer: AnyObject) {
+//        synchronized(self) {
+//            self.observers.remove(observer)
+//        }
+//    }
+//
+//    func isObserved(by object: AnyObject) -> Bool {
+//        return synchronized(self, block: { () -> (Bool) in
+//            return self.observers.contains(object)
+//        })
+//    }
+//
+//    func notify(of state: ModelState) {
+//        synchronized(self) {
+//            self.notifyWith(self.selector(for: state))
+//        }
+//    }
+//
+//    func notifyOfState(_ state: ModelState, with object: AnyObject) {
+//        synchronized(self) {
+//            self.notifyWith(self.selector(for: state), with: object)
+//        }
+//    }
+//
+//    func perform(notification: Bool, block: () -> ()) {
+//        let notify = self.notify
+//        self.notify = notification
+//        block()
+//        self.notify = notify
+//    }
+//
+//    //Method created for overriding, do not call directly
+//    func selector(for state: ModelState) -> Selector? {
+//        return nil
+//    }
+//
+//    // MARK: - Private Functions
+//
+//    private func notifyWith(_ selector: Selector?, with object: AnyObject? = nil) {
+//        if self.notify {
+//            self.observers.allObjects.forEach({ (object) in
+//                if (object.responds(to: selector)) {
+//                    _ = object.perform(selector, with: object)
+//                }
+//            })
+//        }
+//    }
+//}
+//
+//
